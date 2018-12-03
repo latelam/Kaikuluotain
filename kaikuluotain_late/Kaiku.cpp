f100c13678d48d9b7db187947bc6ce08a57f8d4d
unknown <lauri.marttila@student.edupoli.fi>
2018-12-03 10:22:31 +0200

    Tein Ääkköset.

--- kaikuluotain_late/Kaiku.cpp
+++ kaikuluotain_late/Kaiku.cpp
@@ -49,7 +49,11 @@ void Kaiku::NayttaaLCD() {
   
   LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
   lcd.begin(16, 2);
- 
+  lcd.createChar(1, ae);
+  lcd.createChar(2, oo);
+  lcd.createChar(3, AE);
+  lcd.createChar(4, OO);
+
   Mittaa();
   Laskee();
   
@@ -61,13 +65,15 @@ void Kaiku::NayttaaLCD() {
     } else {
     lcd.clear();
     lcd.setCursor (0, 0);
-    lcd.print("Valimatka :");             // ..muutoin näytä etäisyys senttimetreinä.
+    lcd.print("V");             // ..muutoin näytä etäisyys senttimetreinä.
+    lcd.write(byte(1));
+    lcd.print("limatka :");
     lcd.setCursor (0, 1); 
     lcd.print(valimatka);
     lcd.print(" cm");
     }
     delay(viive);   
-} 
+}
 
 void Kaiku::Alkudemo() {
 
